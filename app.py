7daa4b4add70bd124f9af0c0c62db0a1795b6b2d
Dignity <memecu420@gmail.com>
2026-04-05 23:03:17 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_file
+import random
+import os
+
+app = Flask(__name__)
+
+IMAGE_FOLDER = 'images'
+
+@app.route('/qr-code-image')
+def qr_code_image():
+    images = os.listdir(IMAGE_FOLDER)
+    chosen_image = random.choice(images)
+    return send_file(os.path.join(IMAGE_FOLDER, chosen_image))
+
+if __name__ == '__main__':
+    app.run(debug=True)
