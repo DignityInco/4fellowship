90f75c73042448d62fa6a85439a654b2dadbe7a0
DignityInco <memecu420@gmail.com>
2026-09-12 15:17:31 +0100

    by the grace of GOD i will win this battler

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 IMAGE_FOLDER = 'images'
 
-@app.route("https://fourfellowship.onrender.com/qr-code-image")
+@app.route('/')
 def qr_code_image():
     images = os.listdir(IMAGE_FOLDER)
     chosen_image = random.choice(images)
